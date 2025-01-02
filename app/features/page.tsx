18d94c2fe7c68ff0aeb5ac3bd53891dc6718139a
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2025-01-02 18:48:45 +0500

    default styles

---
+++ app/features/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Smart Notes | Features",
+  description: "Smart Notes Features Page",
+}
+export default function Features() {
+  return <h1>Features</h1>
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Smart Notes | Pricing",
+  description: "Smart Notes Pricing Page",
+}
+export default function Pricing() {
+  return <h1>Pricing</h1>
+}
